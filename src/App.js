1e25a33952e98b137cca4bf4aeb8642cfbb91045
samyarsworld <samyarfarjam@outlook.com>
2023-03-11 18:06:01 -0500

    inital frontend commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Login from './components/Login';
+import Register from './components/Register';
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/galactchat/login' element={<Login />}  />
+          <Route path='/galactchat/register' element={<Register />}  />
+        </Routes>
+        
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import './main.scss';
+
+import {Provider} from 'react-redux';
+import store from './store/index.js';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+reportWebVitals();
